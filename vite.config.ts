0ad9214fd9ee803d4918e7317ffc9da76ece22db
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-11-28 14:33:59 -0500

    deleted comments

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: { // Add this section for Vitest configuration
-    globals: true,         // Enables global test functions like 'describe', 'it'
-    environment: 'jsdom',  // Sets up a DOM-like environment for testing React components
-    setupFiles: './src/test/setup.ts', // Optional: Path to test setup file for global mocks, etc.
+  test: {
+    globals: true,        
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts', 
   },
 });
