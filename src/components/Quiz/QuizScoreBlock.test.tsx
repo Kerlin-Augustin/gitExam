7d74dc9e2d38c93e8891b9c5b7b3a434282edea5
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-12-22 23:49:32 -0500

    added react testing library

---
+++ src/components/Quiz/QuizScoreBlock.test.tsx
@@ -0,0 +1,12 @@
+import { describe, it } from "vitest";
+import QuizScoreBlock from "./QuizScoreBlock";
+
+describe('Making sure the score card renders properly', () => {
+  it('Score Card Rneders', () => {
+    // Arrange
+    const sut = 
+
+    // Act
+    // Assert
+  })
+})
