6eca59d57f233faa6e3c27faa4b4f7b634c46b25
Kerlin Augustin <102834611+Kerlin-Augustin@users.noreply.github.com>
2025-01-25 16:56:51 -0500

    Fixing Auth Pages (#8)
    * debugging
    
    * fixed auth issue

--- server.js
+++ server.js
@@ -11,26 +11,19 @@ app.use(cors());
 app.use(express.json());
 
 app.post(`/api/login`, async (req, res) => {
+  console.log('Login route hit');
   const { email, password } = req.body;
   
-  console.log(req.body)
+  console.log('email', email)
+  console.log('password', password)
   
   try {
     const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+
+    console.log('Supabase Response:', { data, error })
     
     if (error) {
       return res.status(400).json({ error: error.message });
-    }
-    
-    // Check if user exists in database
-    const { data: userExists } = await supabase
-    .from("users")
-    .select("*")
-    .eq("email", email)
-    .single();
-    
-    if (!userExists) {
-      throw new Error('User not found. Double check your email or password!')
     }
     
     res.status(200).json({ message: "Login successful", data });
@@ -38,13 +31,6 @@ app.post(`/api/login`, async (req, res) => {
     console.error(error);
     res.status(500).json({ error: "An unexpected error occurred." });
   }
-});
-
-app.use(express.static(path.join(process.cwd(), 'dist')));
-
-// Always have this route last because it is a catch all for rerouting
-app.get('*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'dist', 'index.html'));
 });
 
 // Start the server
