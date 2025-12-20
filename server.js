01d3061d43fee71c7809e3ad93810a7aeaa0fb1d
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-12-20 16:51:37 -0500

    changed port number

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from "express";
-import path from "path";
 import cors from "cors";
 import { supabase } from "./src/lib/supabase.js"; 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
